17771b4c941d77c39c6ffa4887d212cc7fe8f847
Hun <43863985+Owenm05@users.noreply.github.com>
2022-05-17 08:07:00 -0500

    Create Clock.py

---
+++ Clock.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+## credits to https://www.programiz.com/python-programming/datetime/current-time
