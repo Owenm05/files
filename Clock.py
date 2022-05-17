84ecfb37faf50152197335e75d8ccf0fdeadff0f
Hun <43863985+Owenm05@users.noreply.github.com>
2022-05-17 08:09:41 -0500

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -3,6 +3,6 @@ from datetime import datetime
 now = datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
+current_date = now.strftime("%D")
 print("Current Time =", current_time)
-
-## credits to https://www.programiz.com/python-programming/datetime/current-time
+print("Current Date =", current_date)
